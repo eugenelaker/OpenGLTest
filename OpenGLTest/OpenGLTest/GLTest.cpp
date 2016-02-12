f4be1e4e6edca138d2999bacff918359c032f956
Eugene Lakernik <eugene.laker@gmail.com>
2016-02-12 19:25:43 +0200

    running cube

--- OpenGLTest/OpenGLTest/GLTest.cpp
+++ OpenGLTest/OpenGLTest/GLTest.cpp
@@ -1,13 +1,16 @@
 #include <glut.h>
 #include <GL/glu.h>
 
+GLfloat x1 = 0.0; GLfloat y1 = 0.0; GLfloat rsize = 25;
+GLfloat xstep = 1.0; GLfloat ystep = 1.0;
+GLfloat windowWidth; GLfloat windowHeight;
 
 void RenderScene()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(1.0, 1.0, 0.0);
-	glRectf(-25.0, 25.0, 25.0, -25.0);
-	glFlush();
+	glRectf(x1, y1, x1+rsize, y1-rsize);
+	glutSwapBuffers();
 }
 
 void ChangeSize(GLsizei w, GLsizei h)
@@ -24,10 +27,14 @@ void ChangeSize(GLsizei w, GLsizei h)
 	aspectRatio = (GLfloat)w / (GLfloat)h;
 
 	if (w <= h) {
-		glOrtho(-100.0, 100.0, -100.0 / aspectRatio, 100.0 / aspectRatio, -1.0, 1.0);
+		windowWidth = 100;
+		windowHeight = 100 / aspectRatio;
+		glOrtho(-100.0, 100.0, -windowHeight, windowHeight, 1.0, -1.0);
 	}
 	else {
-		glOrtho(-100.0 * aspectRatio, 100.0 * aspectRatio, -100.0 , 100.0 , 1.0, -1.0);
+		windowHeight = 100;
+		windowWidth = 100 * aspectRatio;
+		glOrtho(-windowWidth, windowWidth, -100.0 , 100.0 , 1.0, -1.0);
 	}
 
 	glMatrixMode(GL_MODELVIEW);
@@ -39,12 +46,37 @@ void SetupRC()
 	glClearColor(1.0, 0.0, 0.0, 1.0);
 }
 
+void TimerFuntion(int value)
+{
+	if (x1 > windowWidth - rsize || x1 < -windowWidth)
+		xstep = -xstep;
+	if (y1 > windowHeight || y1 < -windowHeight + rsize)
+		ystep = -ystep;
+
+	x1 += xstep;
+	y1 += ystep;
+
+	if (x1 > (windowWidth - rsize + xstep))
+		x1 = windowWidth - rsize - 1;
+	else if (x1 < -(windowWidth + xstep))
+		x1 = -windowWidth - 1;
+	if (y1 > (windowHeight + ystep))
+		y1 = windowHeight - 1;
+	else if (y1 < -(windowHeight - rsize + ystep))
+		y1 = -windowHeight + rsize - 1;
+
+	glutPostRedisplay();
+	glutTimerFunc(10, TimerFuntion, 1);
+
+}
+
 void main()
 {
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 	glutCreateWindow("OpenGL Test");
 	glutDisplayFunc(RenderScene);
 	glutReshapeFunc(ChangeSize);
+	glutTimerFunc(10, TimerFuntion, 1);
 	SetupRC();
 	glutMainLoop();
 }
